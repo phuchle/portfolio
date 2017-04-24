50071e95cad1cc85e293316f0cded6ff316ea26a
Phuc Le <le.phuc.h@gmail.com>
2017-04-24 16:26:41 -0700

    Add new entry to portfolio.  Clean up some styling and spacing.  Add a background filter for intro text to increase readability.

--- js/script.js
+++ js/script.js
@@ -26,9 +26,9 @@ $(document).ready(function() {
     } // End if
   });
 
-  $(function(){
+  $(function() {
     $('.nav a').on('click', function(){
-      if ($('.navbar-toggle').css('display') != 'none'){
+      if ($('.navbar-toggle').css('display') != 'none') {
         $(".navbar-toggle").trigger("click");
       }
     });
