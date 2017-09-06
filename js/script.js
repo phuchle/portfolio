ef4c1959bc1672cd916f72493277ff8c9c442c22
Phuc Le <le.phuc.h@gmail.com>
2017-09-05 17:39:59 -0700

    Redesigned Head and About sections.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,40 @@
 $(document).ready(function() {
-  // Add scrollspy to <body>
-  $('body').scrollspy({
-    target: ".navbar",
-    offset: 50
+  $('body').scrollspy({ target: '#navbar-example'
   });
 
+  $(function () {
+    $('[data-toggle="popover"]').popover();
+  });
+  // // Add scrollspy to <body>
+  // $('body').scrollspy({
+  //   target: '.navbar',
+  //   offset: 50
+  // });
+  //
   // Add smooth scrolling on all links inside the navbar
-  $("#myNavbar a").on('click', function(event) {
+  // $('#myNavbar a').on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
+    // if (this.hash !== '') {
+    //   event.preventDefault();
+    //   var hash = this.hash;
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function() {
-        window.location.hash = hash;
-      });
-    }
-  });
-
-  $(function() {
-    $('.nav a').on('click', function(){
-      if ($('.navbar-toggle').css('display') != 'none') {
-        $(".navbar-toggle").trigger("click");
-      }
-    });
-  });
+  //     $('html, body').animate({
+  //       scrollTop: $(hash).offset().top
+  //     }, 800, function() {
+  //       window.location.hash = hash;
+  //     });
+  //   }
+  // });
+  //
+  // $(function() {
+  //   $('.nav a').on('click', function(){
+  //     if ($('.navbar-toggle').css('display') != 'none') {
+  //       $('.navbar-toggle').trigger('click');
+  //     }
+  //   });
+  // });
 
 
 });
