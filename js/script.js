4914a30a229d6a5912c7948274218fbb04c7c41e
phuchle <le.phuc.h@gmail.com>
2017-09-06 12:20:46 -0700

    Fix smooth scroll.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,24 @@
 $(document).ready(function() {
-  $('body').scrollspy({ target: '#navbar-example'
-  });
-
+  // activates popover effect on skillset
   $(function () {
     $('[data-toggle="popover"]').popover();
   });
-  // // Add scrollspy to <body>
-  // $('body').scrollspy({
-  //   target: '.navbar',
-  //   offset: 50
-  // });
-  //
-  // Add smooth scrolling on all links inside the navbar
-  // $('#myNavbar a').on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    // if (this.hash !== '') {
-    //   event.preventDefault();
-    //   var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-  //     $('html, body').animate({
-  //       scrollTop: $(hash).offset().top
-  //     }, 800, function() {
-  //       window.location.hash = hash;
-  //     });
-  //   }
-  // });
-  //
-  // $(function() {
-  //   $('.nav a').on('click', function(){
-  //     if ($('.navbar-toggle').css('display') != 'none') {
-  //       $('.navbar-toggle').trigger('click');
-  //     }
-  //   });
-  // });
+  $('a[href^="#"]').on('click', function(e) {
+   e.preventDefault();
 
+   // store hash
+   var hash = this.hash;
+   console.log(hash);
 
+   $('body').animate({
+     scrollTop: $(hash).offset().top
+   }, 800, function() {
+
+     // when done, add hash to url
+     // (default click behaviour)
+     window.location.hash = hash;
+   });
+
+});
 });
